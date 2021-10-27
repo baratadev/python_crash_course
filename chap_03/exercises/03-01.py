c24749f3b3449868cca72e705c959761ea32f12d
Barata <8771928+baratadev@users.noreply.github.com>
2021-10-27 12:25:02 -0300

    finish chapter 3 exercises

---
+++ chap_03/exercises/03-01.py
@@ -0,0 +1,4 @@
+names = ['ernest hemingway', 'guimarães rosa', 'charlotte perkins gilman']
+
+for name in names:
+    print(f"Nice writings, {name.title()}!")
---
+++ chap_03/exercises/03-04.py
@@ -0,0 +1,43 @@
+guests = ['ernest hemingway', 'henry david thoreau',
+          'charlotte perkins gilman', 'clarissa pinkola estés']
+
+print(f"Hello {guests[0].title()}, free food this friday!!!")
+print(f"Hello {guests[1].title()}, free food this friday!!!")
+print(f"Hello {guests[2].title()}, free food this friday!!!")
+print(f"Hello {guests[3].title()}, free food this friday!!!\n")
+
+print(f"{guests[1].title()} don't like free food.\n")
+guests[1] = 'graciliano ramos'
+
+print(f"Hello {guests[0].title()}, free food this friday!!!")
+print(f"Hello {guests[1].title()}, free food this friday!!!")
+print(f"Hello {guests[2].title()}, free food this friday!!!")
+print(f"Hello {guests[3].title()}, free food this friday!!!")
+
+print(f"\nHey, people! I found a bigger table!!!")
+
+guests.insert(0, 'jorge amado')
+guests.insert(2, 'euclides da cunha')
+guests.append('rachel de queiroz')
+
+print(f"\nHello {guests[0].title()}, free food this friday!!!")
+print(f"Hello {guests[1].title()}, free food this friday!!!")
+print(f"Hello {guests[2].title()}, free food this friday!!!")
+print(f"Hello {guests[3].title()}, free food this friday!!!")
+print(f"Hello {guests[4].title()}, free food this friday!!!")
+print(f"Hello {guests[5].title()}, free food this friday!!!")
+print(f"Hello {guests[6].title()}, free food this friday!!!")
+
+print("\nSorry, the table will not arrive on time.")
+
+guests.pop()
+guests.pop()
+guests.pop()
+guests.pop()
+guests.pop()
+
+print(f"\nHello {guests[0].title()}, free food this friday!!!")
+print(f"Hello {guests[1].title()}, free food this friday!!!")
+
+del guests[:]
+print(guests)
