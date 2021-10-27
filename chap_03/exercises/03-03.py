6c9280b2adf3621a38d8f1b51bfff3518675842c
Barata <8771928+baratadev@users.noreply.github.com>
2021-10-27 17:54:12 -0300

    add exercise 3-3. Your Own List

---
+++ chap_03/exercises/03-03.py
@@ -0,0 +1,5 @@
+bicycle_brands = ['surly', 'trek', 'jones']
+
+print(f"{bicycle_brands[0].title()} to ride around town;")
+print(f"{bicycle_brands[1].title()} to do some trails;")
+print(f"{bicycle_brands[2].title()} to travel around the world.")
