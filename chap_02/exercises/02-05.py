1a37aad116795b877b647382ddead48309ffa05e
Barata <8771928+baratadev@users.noreply.github.com>
2021-10-27 12:01:06 -0300

    add exercise 2.5. Famous Quote, and 2.6. Famous Quote 2

---
+++ chap_02/exercises/02-05.py
@@ -0,0 +1,9 @@
+quote = '“Perhaps I should not have been a fisherman, he thought. But that was the thing that I was born for.”'
+
+author = "Ernest Hemingway"
+
+book = "The Old Man and the Sea"
+
+famous_quote = f'{author} wrote in "{book}" book:\n{quote}'
+
+print(famous_quote)
