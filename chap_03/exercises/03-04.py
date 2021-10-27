01f8c7c487b2ff8230f477f51555d12247917877
Barata <8771928+baratadev@users.noreply.github.com>
2021-10-27 18:03:31 -0300

    add exercise 3-4. Guest List w/o a loop

---
+++ chap_03/exercises/03-04.py
@@ -0,0 +1,7 @@
+people = ['ernest hemingway', 'henry david thoreau',
+          'charlotte perkins gilman', ' clarissa pinkola estés']
+
+print(f"Hello {people[0].title()}, free food this friday!!!")
+print(f"Hello {people[1].title()}, free food this friday!!!")
+print(f"Hello {people[2].title()}, free food this friday!!!")
+print(f"Hello {people[3].title()}, free food this friday!!!")
