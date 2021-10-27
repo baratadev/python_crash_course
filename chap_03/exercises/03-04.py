bf35bf3874574a37975f758b74246a8eae4d7e9a
Barata <8771928+baratadev@users.noreply.github.com>
2021-10-27 18:08:10 -0300

    add exercise 3-5. Changing Guest List

--- chap_03/exercises/03-04.py
+++ chap_03/exercises/03-04.py
@@ -1,7 +1,15 @@
-people = ['ernest hemingway', 'henry david thoreau',
+guests = ['ernest hemingway', 'henry david thoreau',
           'charlotte perkins gilman', ' clarissa pinkola estés']
 
-print(f"Hello {people[0].title()}, free food this friday!!!")
-print(f"Hello {people[1].title()}, free food this friday!!!")
-print(f"Hello {people[2].title()}, free food this friday!!!")
-print(f"Hello {people[3].title()}, free food this friday!!!")
+print(f"Hello {guests[0].title()}, free food this friday!!!")
+print(f"Hello {guests[1].title()}, free food this friday!!!")
+print(f"Hello {guests[2].title()}, free food this friday!!!")
+print(f"Hello {guests[3].title()}, free food this friday!!!\n")
+
+print(f"{guests[1].title()} don't like free food.\n")
+guests[1] = 'graciliano ramos'
+
+print(f"Hello {guests[0].title()}, free food this friday!!!")
+print(f"Hello {guests[1].title()}, free food this friday!!!")
+print(f"Hello {guests[2].title()}, free food this friday!!!")
+print(f"Hello {guests[3].title()}, free food this friday!!!")
